7bc2ef616f368018afa831dff0dd8227c11013df
jbsmith86 <v-joelsm@FALCON245.redmond.corp.microsoft.com>
2013-09-08 12:23:00 -0700

    Fixed example files

---
+++ makelists.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import urllib2
+import json
+import os
+from BeautifulSoup import BeautifulSoup
+from baseclass import OkCupid
+
+def getpageusernames(url):
+	request = urllib2.build_opener()
+	request.addheaders.append(OkCupid.cookietuple)
+	returnedpage = request.open(url)
+	usernamehtml = BeautifulSoup(returnedpage.read()).findAll("span", { "class" : "username" })
+	finallist = []
+	for i in usernamehtml:
+		finallist.append(i.text.encode("utf-8"))
+	return finallist
+
+if  __name__ =='__main__':
+	maindir = os.getcwd()
+	queries = json.load(open('queries.json'))
+	for query in queries:
+		filterout = []
+		for i in query['filterout']
+			filterout += OkCupid.getlist(i.encode("utf-8"))
+		currentlist = OkCupid.getlist(query['filepath'].encode("utf-8"))
+		while True:
+			print(query['name'].encode("utf-8") + ' List Count: ' + str(len(currentlist)))
+			matched = []
+			matchcount = 0
+			potentials = getpageusernames(query['queryurl'].encode("utf-8"))
+			for potential in potentials:
+				if potential in filterout:
+					continue
+				else:
+					matchcount += 1
+					matched.append(potential)
+					filterout.append(potential)
+			currentlist = currentlist + matched
+			currentlist.sort()
+			OkCupid.writelisttofile(query['filepath'].encode("utf-8"), currentlist)
+			if matchcount == 0:
+				break
